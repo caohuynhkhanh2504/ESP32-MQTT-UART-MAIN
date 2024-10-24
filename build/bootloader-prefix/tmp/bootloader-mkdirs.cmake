017a3fe5dc5fb592f40b16825e3be979d1471a30
caohuynhkhanh2504 <caohuynhkhanh37@gmail.com>
2024-10-24 18:40:42 +0700

    First commit

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/Espressif/frameworks/esp-idf-v5.3.1/components/bootloader/subproject"
+  "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader"
+  "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader-prefix"
+  "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader-prefix/tmp"
+  "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader-prefix/src"
+  "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/NCKH2024/Summary/Huka/ESP32_MQTT_I2C/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
